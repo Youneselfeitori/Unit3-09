ebe2d19db8c8e13a14a98fe2ec51d8968eebc312
Younes Elfeitori <youneselfeitori@gmail.com>
2017-11-01 22:18:04 -0400

    Init

---
+++ unicode_alphabet.py
@@ -0,0 +1,12 @@
+
+# Created by: Younes Elfeitori
+# Created on: Oct 2017
+# Created for: ICS3U
+# This program shows the alphabets in capital and small fonts
+
+
+for capital_letters in range(65, 91):
+    for small_letters in range(97, 123):
+#output
+     print unichr(capital_letters), unichr(small_letters)
+
